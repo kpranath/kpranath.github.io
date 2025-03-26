d9a9012c3210ff787deb73a3ac0cce7faa67ff05
kpranath@gmail.com <kpranath@gmail.com>
2025-03-26 03:25:40 -0400

    almost done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Education from "./components/Education";
 
 class App extends Component {
 
@@ -136,6 +137,10 @@ class App extends Component {
           resumeProjects={this.state.resumeData.projects}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
+        <Education
+        resumeEducation={this.state.resumeData.education}
+        resumeBasicInfo={this.state.resumeData.basic_info}
+      />
         <Skills
           sharedSkills={this.state.sharedData.skills}
           resumeBasicInfo={this.state.resumeData.basic_info}
